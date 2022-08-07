6c6c8a9db3cf32ee9fc55b850fcd07cb9fa9c516
sophyalves <109874929+sophyalves@users.noreply.github.com>
2022-08-06 21:23:39 -0300

    Add files via upload

---
+++ sophyalves/script.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+const switcher= document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+document.bory.classList.toggle('light-theme');
+document.bory.classList.toggle('dark-theme')
+})
+const Clique = document.bory. Clique;
+if(Clique == "light-theme") {
+this.textContent= "dark";}
+else {
+this.textContent = "light";}
+
+
+
+
